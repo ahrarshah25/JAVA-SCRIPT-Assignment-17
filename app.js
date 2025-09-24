316c0f142544f3277a7d936f3f72080b1a2b44ba
Ahrar Shah <ahrar.09321@gmail.com>
2025-09-24 14:44:00 +0500

    Question 1 & 2 (Only)

---
+++ app.js
@@ -0,0 +1,11 @@
+// Q.1
+// var myArr = [ [] , [] , [] ];
+// console.log(myArr)
+// Q.2
+//  var myArr = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+
+// console.log("Matrix array:", myArr);
